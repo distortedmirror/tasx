6cdaf90887976d55b4c55589494c40d8ff6ec4dd
Mark Brito <mark.brito.gcloud@gmail.com>
2020-12-26 03:58:35 +0000

    Jar CP

--- start.sh
+++ start.sh
@@ -6,7 +6,8 @@ cd vxd
 echo 'Load Tasks (y/n)?'
 read load
 if [ "$load" != "y" -a "$load" != "Y" ]; then
-java -cp /usr/lib/jvm/java-8-openjdk-amd64/lib/tools.jar:.. vxd.vxd 
+java -cp 
+../crimson-1.1.3.jar:/usr/lib/jvm/java-8-openjdk-amd64/lib/tools.jar:.. vxd.vxd 
 else
-java -cp /usr/lib/jvm/java-8-openjdk-amd64/lib/tools.jar:.. vxd.vxd ./Work/Tasks/Tasks.xml
+java -cp ../crimson-1.1.3.jar:/usr/lib/jvm/java-8-openjdk-amd64/lib/tools.jar:.. vxd.vxd ./Work/Tasks/Tasks.xml
 fi

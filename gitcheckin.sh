0d1d07958ee6dc873ea8bcec3934336ea3f2979a
Mark Brito <mark.brito@gmail.com>
2021-02-06 08:51:20 -0700

    Encryption of XML

--- gitcheckin.sh
+++ gitcheckin.sh
@@ -1,1 +1,7 @@
+
+echo 'Delete decrypted XML (y/n)?'
+read del
+if [ "$del" != "y" -a "$del" != "Y" ]; then
+	for i in `find . -name '*.decrypted'`; do rm $i; done
+fi
 git config --global user.email "mark.brito@gmail.com"; git config --global user.name "Mark Brito"; git add .; git commit; git push
--- vxd/makeall.sh
+++ vxd/makeall.sh
@@ -1,1 +1,1 @@
-for i in `find . -name '*.java'`; do javac -cp /usr/lib/jvm/java-8-openjdk-amd64/lib/tools.jar:.. $i; done
+for i in `find . -name '*.java'`; do javac -g -cp /usr/lib/jvm/java-8-openjdk-amd64/lib/tools.jar:.. $i; done

1fcf8502c747738647b0f7d7cda2f3e38d880e6c
Mark Brito <mark.brito@gmail.com>
2021-02-08 00:34:22 -0700

    ok

--- gitcheckin.sh
+++ gitcheckin.sh
@@ -2,12 +2,12 @@
 echo 'Delete decrypted XML (y/n)?'
 read del
 if [ "$del" != "y" -a "$del" != "Y" ]; then
-	for i in `find . -name '*.decrypted'`; do rm $i; done
-	for i in `find . -name '*.unencrypted'`; do rm $i; done
+	for i in `find . -name '*.decrypted'`; do echo rm $i; done
+	for i in `find . -name '*.unencrypted'`; do echo rm $i; done
 fi
 echo 'Replace new encrypted XML to main XML(y/n)?'
 read repl
 if [ "$repl" != "y" -a "$repl" != "Y" ]; then
-	for i in `find . -name '*.encrypted'`; do cp $i `echo $i|perl -pe 's/.encrypted//g'`; done
+	for i in `find . -name '*.encrypted'`; do echo cp $i `echo $i|perl -pe 's/.encrypted//g'`; done
 fi
 git config --global user.email "mark.brito@gmail.com"; git config --global user.name "Mark Brito"; git add .; git commit; git push

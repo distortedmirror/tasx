1ad9f52ad803126c6e8d75c6f9bb9734fe722dca
Mark Brito <mark.brito@gmail.com>
2021-04-28 19:24:09 +0000

    start script

--- start.sh
+++ start.sh
@@ -1,9 +1,3 @@
 export DISPLAY=:1
 cd vxd
-echo 'Load Tasks (y/n)?'
-read load
-if [ "$load" != "y" -a "$load" != "Y" ]; then
-java -cp ../crimson-1.1.3.jar:/usr/lib/jvm/java-8-openjdk-amd64/lib/tools.jar:.. vxd.vxd $1
-else
-jdb -cp ../crimson-1.1.3.jar:/usr/lib/jvm/java-8-openjdk-amd64/lib/tools.jar:.. vxd.vxd ./Work/Tasks/Tasks.xml
-fi
+. start.sh

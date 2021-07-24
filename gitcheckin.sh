a5dc2c0e2a2cbaa33f10ebcaea4cff93141705ec
Mark Brito <mark.brito@gmail.com>
2021-07-24 22:14:32 +0000

    ok

--- gitcheckin.sh
+++ gitcheckin.sh
@@ -1,4 +1,4 @@
-for i in `find . -name '*.decrypted'`; do rm $i; done
-for i in `find . -name '*.unencrypted'`; do rm $i; done
+#for i in `find . -name '*.decrypted'`; do rm $i; done
+#for i in `find . -name '*.unencrypted'`; do rm $i; done
 #or i in `find . -name '*.encrypted'`; do echo cp $i `echo $i|perl -pe 's/.encrypted//g'`; done
 git config --global user.email "mark.brito@gmail.com"; git config --global user.name "Mark Brito"; git add .; git commit; git push

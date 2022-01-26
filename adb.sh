3eb66ab8af013bcc1176a70329449e6f7e361e58
Mark Brito <mark.brito@gmail.com>
2022-01-26 12:51:22 -0800

    Commit

---
+++ adb.sh
@@ -0,0 +1,3 @@
+adb devices
+echo Forwarding 1080 Turn on SOCKS proxy on Android
+adb forward tcp:1080 tcp:1080
--- sshtun.sh
+++ sshtun.sh
@@ -1,1 +1,1 @@
-echo markbrito1234;ssh -D 127.0.0.1:8888 markbrito-vpnjantit.com@au1.vpnjantit.com -N
+echo markbritokorea;ssh -D 127.0.0.1:8888 markbritokorea-vpnjantit.com@kr1.vpnjantit.com -N

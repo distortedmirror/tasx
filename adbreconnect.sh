53eb8a9706ec774d94652988b50470fad12c9382
Mark Brito <mark.brito@gmail.com>
2022-02-17 08:56:25 -0800

    Commit

---
+++ adbreconnect.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+#set -o xtrace
+
+PROGNAME=$(basename $0)
+die () {
+  echo "${PROGNAME}: ${1:-"Unknown Error, Abort."}" 1>&2
+  exit 1
+}
+status=0
+./adb.sh &
+while [[ 1 ]]; do
+  curl -sSf --socks5-hostname localhost:1080 www.google.com > /dev/null || status=$?
+  if [[ $status -ne 0 ]]; then
+if [ [ `pgrep $!` ] && 1 || 0 ]; then
+kill -9 $!
+fi
+./adb.sh &
+    echo "Trying to reconnect .."
+    # kill proxy
+    # reconnect-cmd || die "$LINENO: reconnecting failed"
+  fi
+  sleep 15
+done

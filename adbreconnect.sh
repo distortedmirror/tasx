eb4ff2bff05a604f83ef17932b8c55273f8cc1bf
Mark Brito <mark.brito@gmail.com>
2022-02-21 16:35:15 -0800

    Commit

--- adbreconnect.sh
+++ adbreconnect.sh
@@ -14,7 +14,7 @@ status=0
 while [[ 1 ]]; do
   curl -sSf --socks5-hostname localhost:1080 www.google.com > /dev/null || status=$?
   if [[ $status -ne 0 ]]; then
-if [ `pgrep $! && 1 || 0` ]; then
+if [ `pgrep $!` ]; then
 kill -9 $!
 fi
 ./adb.sh &

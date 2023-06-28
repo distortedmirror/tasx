684623b5ea99f57f9d4ff30f3ed1be79f0b177f2
Your Name <you@example.com>
2023-06-28 10:33:03 -0700

    sh

---
+++ xvnc.sh
@@ -0,0 +1,9 @@
+export DISPLAY=:1
+vncserver --kill
+/data/data/com.termux/files/usr/bin/Xvnc :1 -auth /data/data/com.termux/files/home/.Xauthority -desktop localhost:1 -fp /data/data/com.termux/files/usr/share/fonts/75dpi -geometry 1600x720 -rfbauth /data/data/com.termux/files/home/.vnc/passwd -rfbport 5901 -rfbwait 30000 &
+xfwm4 &
+sleep 2
+firefox --no-remote &
+xterm -rv &
+cd tasx
+./start.sh

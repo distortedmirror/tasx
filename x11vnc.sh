2fc291c697cf36a3b2a57b2f248c0ca0be897800
mark brito <mark.brito@gmail.com>
2023-06-29 01:21:43 -0700

    x11vnc

--- x11vnc.sh
+++ x11vnc.sh
@@ -1,4 +1,4 @@
- export DISPLAY=:1;x11vnc --loop --forever -rfbport 5901 -display :1 &       [1] 
+ export DISPLAY=:1;x11vnc --loop --forever -rfbauth ~/.vnc/vncpasswd -rfbport 5901 -display :1 &       [1] 
 
 
 

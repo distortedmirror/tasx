f8fa956a6e8f33a6dd9dd0168bcac8ef2cbfc430
Mark Brito <mark.brito@gmail.com>
2024-07-21 15:55:44 -0700

    Commit

--- x11vnc.sh
+++ x11vnc.sh
@@ -1,4 +1,4 @@
- export DISPLAY=:0;x11vnc --loop --forever -auth guess -rfbauth ~/.vnc/vncpasswd -rfbport 5900 -display :0 &       [1] 
+ export DISPLAY=:0;x11vnc --loop --forever -rfbauth ~/.vnc/passwd -rfbport 5900 -display :0 &       [1] 
 
 
 
